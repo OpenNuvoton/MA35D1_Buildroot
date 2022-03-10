95ba2e7d03461ff2958914784e5173ec68ef283b
schung <schung@nuvoton.com>
2022-03-10 10:28:17 +0800

    Update post-build.sh
    Change-Id: Ice40134373cf6cdcff081a66ed80a46afce753af

--- board/nuvoton/ma35d1/post-build.sh
+++ board/nuvoton/ma35d1/post-build.sh
@@ -5,7 +5,7 @@ MODULES_TDIR=$TARGET_DIR/lib/modules/5.4.181
 GFXDRIVERS_TDIR=$TARGET_DIR/usr/lib/directfb-1.7-7/gfxdrivers
 
 if grep -Eq "^BR2_PACKAGE_BUSYBOX=y$" ${BR2_CONFIG}; then
-	install -d -m 755 ${MODULES_TARGET_DIR}
+	install -d -m 755 ${MODULES_TARGET_TDIR}
 	install -d -m 755 ${GFXDRIVERS_TDIR}
 	cp ${MODULES_DIR}/*.ko ${MODULES_TDIR}/
 	cp ${MODULES_DIR}/libdirectfb_gal.so ${GFXDRIVERS_TDIR}/
