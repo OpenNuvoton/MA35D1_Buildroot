b670fadaf58172cda8643759c4c9fd33d348accd
schung <schung@nuvoton.com>
2022-03-17 16:45:17 +0800

    Updated modules.sh
    Fix sometimes failing to automatically insmod
    vc8000.ko / galcore.ko /dcultrafb.ko
    
    Change-Id: I73fef015615adcab3f4d25d469f43cf99af1e6de

--- board/nuvoton/ma35d1/modules/modules.sh
+++ board/nuvoton/ma35d1/modules/modules.sh
@@ -1,3 +1,3 @@
-modprobe dcultrafb
-modprobe galcore
-modprobe ma35d1-vc8000
+insmod /lib/modules/`uname -r`/dcultrafb.ko
+insmod /lib/modules/`uname -r`/galcore.ko
+insmod /lib/modules/`uname -r`/ma35d1-vc8000.ko
