d68c49cca1352a2e2a74c61d08679e95010fc724
schung <schung@nuvoton.com>
2023-04-12 15:35:47 +0800

    Add SD auto-resizes to Max on first boot.
    Change-Id: I0fb5b48c33acef9c708428789e4cd049eaa6a724

--- board/nuvoton/ma35d1/post-build.sh
+++ board/nuvoton/ma35d1/post-build.sh
@@ -9,6 +9,20 @@ if grep -Eq "^BR2_LINUX_KERNEL_MA35D1_5_10_VERSION=y$" ${BR2_CONFIG}; then
 	MODULES_TDIR=$TARGET_DIR/lib/modules/5.10.140
 fi
 
+
+RESIZE_FILE=${TARGET_DIR}/etc/init.d/S50resize
+cp $MODULES_DIR/../../resize.sh ${TARGET_DIR}/etc/
+if [ -f ${RESIZE_FILE} ]; then
+        rm ${RESIZE_FILE}
+fi
+if grep -Eq "^BR2_MA35D1_RESIZE_SD_MAX=y$" ${BR2_CONFIG}; then
+	export $(grep "BR2_MA35D1_RESIZE_DISK_DRIVE=" $BR2_CONFIG | sed 's/\"//g')
+	export $(grep "BR2_MA35D1_RESIZE_DISK_NUM=" $BR2_CONFIG | sed 's/\"//g')
+	echo "#!/bin/sh" >> ${RESIZE_FILE}
+	echo "/etc/resize.sh ${BR2_MA35D1_RESIZE_DISK_DRIVE} ${BR2_MA35D1_RESIZE_DISK_NUM}" >> ${RESIZE_FILE}
+	chmod 755 ${RESIZE_FILE}
+fi
+
 if grep -Eq "^BR2_PACKAGE_BUSYBOX=y$" ${BR2_CONFIG}; then
 	install -d -m 755 ${MODULES_TARGET_TDIR}
 	install -d -m 755 ${GFXDRIVERS_TDIR}
