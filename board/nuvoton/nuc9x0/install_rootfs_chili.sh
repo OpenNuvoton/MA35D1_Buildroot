bf76266eded4671444699c81ccc1e970eb0c8677
schung <schung@nuvoton.com>
2023-12-04 10:50:49 +0800

    Updated install_rootfs_chili.sh
    Fixed nuc980 chili post-build issue in select systemd
    
    Change-Id: Id567e4ba7bc44a3ce26a6f2e31d29d47c25cea48

--- board/nuvoton/nuc9x0/install_rootfs_chili.sh
+++ board/nuvoton/nuc9x0/install_rootfs_chili.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 rm output/target/etc/resolv.conf
-cp -af -r board/nuvoton/nuc9x0/rootfs-chili/ output/target/
+if grep -Eq "^BR2_INIT_BUSYBOX=y$" ${BR2_CONFIG}; then
+	cp -af board/nuvoton/nuc9x0/rootfs-chili/* output/target/
+fi
 
