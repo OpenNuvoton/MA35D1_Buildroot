61f76d344efeaf9a19a4eae82e8656796dc75352
yclu4 <yclu4@nuvoton.com>
2024-09-09 18:47:23 +0800

    Add a script to support IPI functionality for AMP.
    Change-Id: Id7e34e50aa88cfa0f34a1d72b81bf7c32587e475

---
+++ linux/ampipi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if the DTS file is provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 path/to/file.dtsi"
+    exit 1
+fi
+
+# Path to the DTS file
+DTS_FILE=$1
+
+# Check if the amp node exists
+if grep -q "amp:" "$DTS_FILE"; then
+    echo "Error: AMP node not found!"
+fi
+
+# 1. Change status of amp to 'okay'
+sed -i '/amp: amp {/,/status =/s/status = "disabled";/status = "okay";/' $DTS_FILE
+
+# 2. Extract rxipi and txipi node references
+RXIPI=$(grep -oP '(?<=rxipi = <&)[^>]*' $DTS_FILE)
+TXIPI=$(grep -oP '(?<=txipi = <&)[^>]*' $DTS_FILE)
+
+# 3. Change status of rxipi to 'okay'
+if [ -n "$RXIPI" ]; then
+    sed -i "/$RXIPI:/,/status =/s/status = .*/status = \"okay\";/" $DTS_FILE
+else
+    echo "Error: rxipi not found!"
+fi
+
+# 4. Change status of txipi to 'okay'
+if [ -n "$TXIPI" ]; then
+    sed -i "/$TXIPI:/,/status =/s/status = .*/status = \"okay\";/" $DTS_FILE
+else
+    echo "Error: txipi not found!"
+fi
+
+echo "Updated $DTS_FILE with 'okay' status for amp, $RXIPI, and $TXIPI."
--- linux/linux.mk
+++ linux/linux.mk
@@ -399,6 +399,8 @@ ifeq ($(BR2_LINUX_KERNEL_DTB_IS_SELF_BUILT),)
 define LINUX_BUILD_DTB
 	$(TOPDIR)/linux/dts-reserve \
 		$(LINUX_ARCH_PATH)/boot/dts/nuvoton/$(MA35).dtsi
+	$(if $(BR2_TARGET_ARM_TRUSTED_FIRMWARE_LOAD_A35_WITH_IPI),$(TOPDIR)/linux/ampipi.sh \
+		$(LINUX_ARCH_PATH)/boot/dts/nuvoton/$(MA35).dtsi)
 	$(if $(BR2_TARGET_ARM_TRUSTED_FIRMWARE_LOAD_A35),$(TOPDIR)/linux/dts-reserve \
 		$(LINUX_ARCH_PATH)/boot/dts/nuvoton/$(MA35).dtsi \
 		$(strip $(BR2_TARGET_ARM_TRUSTED_FIRMWARE_LOAD_A35_BASE)) \
@@ -639,3 +641,4 @@ linux-rebuild-with-initramfs:
 	$(call LINUX_INSTALL_IMAGE,$(BINARIES_DIR))
 	# If there is a .ub file copy it to the final destination
 	test ! -f $(LINUX_IMAGE_PATH).ub || cp $(LINUX_IMAGE_PATH).ub $(BINARIES_DIR)
+
